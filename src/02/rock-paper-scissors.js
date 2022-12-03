a48d631ff4dfb84c8f3ab6f3d58189edb8cd5ad8
Andrew Chart <achart@hotmail.com>
2022-12-03 11:15:13 +0000

    Day 2 - Rock Paper Scissors - Part 2

--- src/02/rock-paper-scissors.js
+++ src/02/rock-paper-scissors.js
@@ -6,17 +6,20 @@ const Shape = require('./shapes.js').Shape;
    
     const INPUT_DATA_PATH = './src/02/input.txt';
 
-    let totalScore = 0;
+    let totalScoreOriginalStrategy = 0;
+    let totalScoreUpdatedStraregy = 0;
 
     // Iterate through the whole input file line by line.
     lineReader.eachLine(INPUT_DATA_PATH, (line, last) => {
 
-        totalScore += getRoundScore(line);
+        totalScoreOriginalStrategy += getRoundScore(line, 1);
+        totalScoreUpdatedStraregy  += getRoundScore(line, 2);
     
         // On the last run, print the answers
         if(last) {
 
-            console.log("The answer to Part One is:", totalScore);
+            console.log("The answer to Part One is:", totalScoreOriginalStrategy);
+            console.log("The answer to Part Two is:", totalScoreUpdatedStraregy);
             return;
 
         }
@@ -26,28 +29,58 @@ const Shape = require('./shapes.js').Shape;
 })();
 
 /**
- * Get the score for a given strategy 
- * @param   {String} strategy Strategy expressed as a string e.g. "A X"
- * @returns {Number}          Score associated with this strategy
+ * Get the score for a given strategy and input
+ * @param   {String}  input      Input to drive outcome, expressed as a
+ *                               string e.g. "A X".
+ * @param   {Integer} strategy   Number representing how to interpret the 
+ *                               input. 1 for part one, 2 for part two.
+ * @returns {Number}             Score associated with this strategy + input.
  */
-function getRoundScore(strategy) {
+function getRoundScore(input, strategy) {
     
-    let theirShape = new Shape(strategy.substring(0,1));
-    let yourShape = new Shape(strategy.substring(2,3));
+    let theirShape = new Shape(input.substring(0,1));
+    let yourShape;
+
+    // Work out your shape depending on the strategy we're following
+    if(strategy === 1) {
+        // Your shape depends on the input
+        yourShape = new Shape(input.substring(2,3));
+    } 
     
-    // You Draw
+    if(strategy === 2) {
+        // Your shape depends on the desired outcome
+        switch(input.substring(2,3)) {
+
+            // You want to lose
+            case 'X':
+                yourShape = new Shape(theirShape.beatsShapes[0]);
+                break;
+
+            // You want to draw
+            case 'Y':
+                yourShape = new Shape(theirShape.name);
+                break;
+            
+            // You want to win
+            case 'Z':
+                yourShape = new Shape(theirShape.beatenByShapes[0]);
+                break;
+        }
+    }
+
+    // Actual outcome: You Draw
     if(theirShape.name === yourShape.name) {
         return yourShape.score + 3;
     } 
     
-    // You Win
-    else if(yourShape.beats(theirShape)) {
-        return yourShape.score + 6;
+    // Actual outcome: You Lose
+    else if(yourShape.isBeatenBy(theirShape)) {
+        return yourShape.score + 0;
     }
 
-    // You Lose
+    // Actual outcome: You Win
     else {
-        return yourShape.score + 0;
+        return yourShape.score + 6;
     }
 
 }
--- src/02/shapes.js
+++ src/02/shapes.js
@@ -8,32 +8,39 @@ function Shape(id) {
     this.name = '';
     this.score = 0;
     this.beatsShapes = [];
+    this.beatenByShapes = [];
 
-    this.beats = (oppositionShape) => {
-        if(this.beatsShapes.includes(oppositionShape.name)) return true;
+    this.isBeatenBy = (oppositionShape) => {
+        if(this.beatenByShapes.includes(oppositionShape.name)) return true;
         return false;
     }
 
     switch(id) {
         case 'A':
         case 'X': 
+        case 'rock':
             this.name = 'rock';
             this.score = 1;
-            this.beatsShapes = ['scissors']
+            this.beatsShapes = ['scissors'];
+            this.beatenByShapes = ['paper'];
             break;
 
         case 'B':
         case 'Y': 
+        case 'paper':
             this.name = 'paper';
             this.score = 2;
             this.beatsShapes = ['rock'];
+            this.beatenByShapes = ['scissors'];
             break;
 
         case 'C':
         case 'Z': 
+        case 'scissors':
             this.name = 'scissors';
             this.score = 3;
             this.beatsShapes = ['paper'];
+            this.beatenByShapes = ['rock'];
             break;
     }
 }
