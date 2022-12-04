525b505127407ba4a34f6d0d9ebe9e4a0e595215
Andrew Chart <achart@hotmail.com>
2022-12-04 14:13:53 +0000

    Day 4 - Camp Cleanup - Part 2

--- src/04/camp-cleanup.js
+++ src/04/camp-cleanup.js
@@ -7,6 +7,9 @@ const lineReader = require('line-reader');
     // Part 1 - Track the number of pairs that have ranges that overlap one another fully
     let assignmentPairsOverlappedFully = 0;
 
+    // Part 2 - Track the number of pairs that have ranges that have any overlap including full overlap
+    let assignmentPairsOverlappedAtAll = 0;
+
     // Iterate through the whole input file line by line.
     lineReader.eachLine(INPUT_DATA_PATH, (line, last) => {
         
@@ -15,11 +18,14 @@ const lineReader = require('line-reader');
         if(assignmentsOverlapFully(elfPair[0], elfPair[1]))
             assignmentPairsOverlappedFully++;
 
+        if(assignmentsOverlapAtAll(elfPair[0], elfPair[1]))
+            assignmentPairsOverlappedAtAll++;
+
         // On the last run, print the answers
         if(last) {
 
             console.log("The answer to Part One is:", assignmentPairsOverlappedFully);
-            console.log("The answer to Part Two is:");
+            console.log("The answer to Part Two is:", assignmentPairsOverlappedAtAll);
             return;
 
         }
@@ -87,5 +93,37 @@ function assignmentsOverlapFully(assignment1, assignment2) {
     // If the difference array is empty, the shorter array is fully overlapped
     // by the longer array.
     return (difference.length === 0) ? true : false;
+
+}
+
+
+/**
+ * Compare two arrays and determine whether any elements are common between the two.
+ * @param {Array} assignment1 A set of unique elements in ascending numerical order.
+ * @param {Array} assignment2 Ditto.
+ */
+ function assignmentsOverlapAtAll(assignment1, assignment2) {
+
+    let assignmentsOverlapAtAll = false;
+    
+    // We'll iterate over the shorter array (just to save work, this time!)
+    const assignmentToIterate = (assignment1.length < assignment2.length) 
+        ? 0  // Iterate over the first array if it is shorter
+        : 1; // Iterate over the second array if it is shorter (or the same)
+
+    const longerArray = (assignmentToIterate === 0) ? 1 : 0;
+
+    // We'll loop over the shorter array. As soon as we find an item that's common, 
+    // we know there is overlap and can break the loop / return true.
+    arguments[assignmentToIterate].forEach((item) => {
+        if(arguments[longerArray].includes(item)) {
+            assignmentsOverlapAtAll = true;
+            return;
+        }
+    });
+
+    // If we get here with no common items found, the var remains false; there
+    // is no overlap
+    return assignmentsOverlapAtAll;
 
 }
