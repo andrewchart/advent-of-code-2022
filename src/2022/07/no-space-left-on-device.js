198e7347dd104869681f6538b0ce794733460e6a
Andrew Chart <achart@hotmail.com>
2022-12-11 15:23:25 +0000

    Day 7 - No Space Left On Device - Part 2

--- src/2022/07/no-space-left-on-device.js
+++ src/2022/07/no-space-left-on-device.js
@@ -9,9 +9,10 @@ const FileSystem = require('./file-system.js').FileSystem;
     const input = fs.readFileSync(INPUT_DATA_PATH).toString();
 
     let fileSysModel = new FileSystem(input);
+    let searchParams, searchResults;
 
-    // Part 1: Search for directories with size <= 100000
-    const searchParams = {
+    // Part 1: Search for directories with size <= 100000 and sum their sizes
+    searchParams = {
         fsObjTypes: ["Dir"],
         size: {
             min: 0,
@@ -19,7 +20,7 @@ const FileSystem = require('./file-system.js').FileSystem;
         }
     }
 
-    let searchResults = fileSysModel.getTree().searchDir(searchParams, true);
+    searchResults = fileSysModel.getTree().searchDir(searchParams, true);
     
     let sumDirSizes = searchResults.reduce((totalSize, currentDir) => {
         return totalSize + currentDir.size;
@@ -27,5 +28,34 @@ const FileSystem = require('./file-system.js').FileSystem;
 
     console.log("The answer to Part One is:", sumDirSizes);
 
-})();
 
+    // Part 2: Find the smallest directory that could be deleted
+    // to free up enough space for the update.
+    const DISK_SPACE = 70000000;
+    const USED_SPACE = fileSysModel.getTree().size;
+    const REQD_SPACE = 30000000;
+
+    let minDirSizeToDelete = REQD_SPACE - (DISK_SPACE - USED_SPACE);
+    
+    searchParams = {
+        fsObjTypes: ["Dir"],
+        size: {
+            min: minDirSizeToDelete,
+            max: Infinity
+        }
+    }
+
+    searchResults = fileSysModel.getTree().searchDir(searchParams, true);
+
+    // Reduce results to the smallest dir
+    let dirToDelete = searchResults.reduce((prevDir, currentDir) => {
+        if(prevDir.size <= currentDir.size) {
+            return prevDir;
+        } else {
+            return currentDir;
+        }
+    });
+
+    console.log("The answer to Part Two is:", dirToDelete.size);
+
+})();
