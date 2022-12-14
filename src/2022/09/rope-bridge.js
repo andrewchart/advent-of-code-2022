c92ec2266b0b281b543ca783aa4fac1b02476122
Andrew Chart <achart@hotmail.com>
2022-12-14 18:21:50 +0000

    Day 9 - Rope Bridge - Part 2

--- src/2022/09/rope-bridge.js
+++ src/2022/09/rope-bridge.js
@@ -5,40 +5,48 @@ const Rope = require('./rope.js').Rope;
 (() => {
    
     const INPUT_DATA_PATH = './src/2022/09/input.txt';
+    
+    let ropes = [new Rope(2), new Rope(10)];
 
-    let shortRope = new Rope(1);
-    let shortRopePositionsVistedByTail = [];
+    ropes.forEach((rope, index) => {
 
-    //Iterate through the whole input file, parsing the input
-    lineReader.eachLine(INPUT_DATA_PATH, function(line, last) {
+        let tail = rope.knots[rope.knots.length - 1];
+        let positionsVistedByTail = [];
 
-        [direction, amount] = line.split(' ');
+        //Iterate through the whole input file, parsing the input
+        lineReader.eachLine(INPUT_DATA_PATH, function(line, last) {
 
-        // Part 1: We move step by step and capture the tail 
-        // position every time, since we need to know all the
-        // grid positions it has visited, not just its resting 
-        // position following each move.
-        for(let i = 0; i < amount; i++) {
-            shortRope.moveHead(direction);
+            [direction, amount] = line.split(' ');
 
-            // Turn the co-ordinates into a json string
-            coordString = JSON.stringify(shortRope.tailPos);
+            // We move step by step and capture the tail position 
+            // every time, since we need to know all the grid 
+            // locations it has visited, not just its resting 
+            // position following each move.
+            for(let i = 0; i < amount; i++) {
+                
+                rope.moveHead(direction);
 
-            // If they haven't been visited before, add them to the array
-            if(!shortRopePositionsVistedByTail.includes(coordString)) {
-                shortRopePositionsVistedByTail.push(coordString);
+                // Turn the co-ordinates into a json string
+                coordString = JSON.stringify(tail.pos);
+
+                // If they haven't been visited before, add them to the array
+                if(!positionsVistedByTail.includes(coordString)) {
+                    positionsVistedByTail.push(coordString);
+                }
+
             }
-        }
-        
-        // On the last run, deliver the answers
-        if(last) {
             
-            console.log("The answer to Part One is:", shortRopePositionsVistedByTail.length);
-            console.log("The answer to Part Two is:");
-            return;
+            // On the last run, deliver the answers
+            if(last) {
+                
+                let part = (index === 0) ? "One" : "Two";
+                console.log(`The answer to Part ${part} is:`, positionsVistedByTail.length);
+                return;
 
-        }
+            }
+
+        });
 
     });
-
+    
 })();
--- src/2022/09/rope.js
+++ src/2022/09/rope.js
@@ -1,116 +1,111 @@
+/**
+ * Models a Rope consisting of n Knots.
+ */
 class Rope {
 
-    constructor(length) {
-        this.headPos = { x: 0, y: 0 };
-        this.tailPos = { x: 0, y: 0 };
-        this.length = parseInt(length);
+
+    constructor(knots) {
+        this.knots = [];
+        this.sectionLength = 1; // Distance between two knots is always
+                                // one grid hop, in this puzzle
+
+        for(let i = 0; i < knots; i++) {
+            this.knots.push(new Knot(0, 0));
+        }
     }
+
 
     /**
      * Move the head one step in the given direction
-     * and reposition the tail accordingly.
+     * then the reposition of the following knots.
      * @param   {String} direction L|R|U|D
-     * @returns {Object}           New head position
+     * @returns {Object}           New position for the head
      */
     moveHead(direction) {
 
+        let head = this.knots[0];
+
         switch(direction) {
             case 'L':
-                this.headPos.x--;
+                head.pos.x--;
                 break;
 
             case 'R':
-                this.headPos.x++;
+                head.pos.x++;
                 break;
 
             case 'U':
-                this.headPos.y++;
+                head.pos.y++;
                 break;
 
             case 'D':
-                this.headPos.y--;
+                head.pos.y--;
                 break;
         }
 
-        this.moveTail(direction);
+        this.followHead();
 
-        return this.headPos;
+        return head.pos;
 
     }
 
     /**
-     * Moves the tail relationally to the head
-     * @param   {String} directionOfTravel L|R|U|D
-     * @returns {Object}                   New tail position
+     * After the head has been moved L/R/U/D, reposition all other
+     * knots accordingly, one by one.
+     * 
+     * @returns {Object} Position of the final knot in the rope
      */
-    moveTail(directionOfTravel) {
+    followHead() {
 
-        // If the rope isn't taut yet, the tail stays where it is
-        // This also covers the scenario where the head and tail
-        // overlap.
-        if(!this.isTaut()) return this.tailPos;
+        for(let i = 0; i < this.knots.length; i++) {
 
-        // Otherwise the tail moves in relation to the head
-        switch(direction) {
+            let leadKnot  = this.knots[i]; 
+            let chaseKnot = this.knots[i+1];
 
-            // If the head has gone right, the tail will be to the left of it 
-            // (tx < hx). We determined the rope was taut, so the tail will be
-            // (len) steps away from the head. 
+            // We've reached the last knot
+            if(typeof chaseKnot === "undefined") return leadKnot.pos;
 
-            // Because diagonal differences (hx <> tx && hy <> ty) result in 
-            // the tail following into the same row (for left/right movements)
-            // or the same column (for up/down movements), we update the tail
-            // to be in the same row as the head, in the case of an L or an R.
-            case 'L':
-                this.tailPos.x = this.headPos.x + this.length;
-                this.tailPos.y = this.headPos.y;
-                break;
+            // The two knots are touching so the chaser doesn't move (so 
+            // none of the other knots do either).
+            if(this.isTouching(leadKnot, chaseKnot)) return chaseKnot.pos;
 
-            case 'R':
-                this.tailPos.x = this.headPos.x - this.length;
-                this.tailPos.y = this.headPos.y;
-                break;
+            // Otherwise calculate and set the new position of the chaser
+            let xDiff = leadKnot.pos.x - chaseKnot.pos.x;
+            let yDiff = leadKnot.pos.y - chaseKnot.pos.y;
 
-            case 'U':
-                this.tailPos.x = this.headPos.x;
-                this.tailPos.y = this.headPos.y - this.length;
-                break;
+            // The combined effect of running these 4 adjustments (including 
+            // doing nothing to the x/y pos if the diff is already 0) will
+            // result in the correct movement of the chase knot.
+            if(yDiff <= -1) chaseKnot.pos.y--;
+            if(yDiff >=  1) chaseKnot.pos.y++;
+            if(xDiff <= -1) chaseKnot.pos.x--;
+            if(xDiff >=  1) chaseKnot.pos.x++;
 
-            case 'D':
-                this.tailPos.x = this.headPos.x;
-                this.tailPos.y = this.headPos.y + this.length;
-                break;
         }
 
-        return this.tailPos;
+        return chaseKnot.pos;
 
     }
 
-    /**
-     * I anticipate that part 2 might involve lengthening the rope! 😂
-     * This function returns true if the latest head position infers
-     * that the tail has to move. 
-     * 
-     * Examples below.
-     * 
-     *   len=1       len=1       len=2       len=2       len=2
-     * .........   .........   .........   .....T...   .........
-     * .........   .........   ......T..   .........   .........
-     * ...T.....   ......T..   .........   .........   .........
-     * ...H.....   ....H....   ....H....   ....H....   ....HT...
-     * .........   .........   .........   .........   .........
-     *  (false)      (true)     (false)     (true)      (false)
-     * 
-     * @returns {Boolean}
-     */
-    isTaut() {
 
-        // If the x or y co-ordinate or both of the tail is a 
-        // distance from the head of > length of the rope, the
-        // rope is taut and the tail must move to follow the head.
-        if(
-            Math.abs(this.headPos.x - this.tailPos.x) > this.length ||
-            Math.abs(this.headPos.y - this.tailPos.y) > this.length
+    /**
+     * Determines whether two knots are "touching" or "overlapping 
+     * per the puzzle definition
+     * 
+     * @param   {Knot}     knot1  An instance of Knot
+     * @param   {Knot}     knot2 
+     * @returns {Boolean}         `true` if the two knots are 0 or 
+     *                            1 hops apart
+     */
+    isTouching(knot1, knot2) {
+
+        let xDiff = Math.abs(knot1.pos.x - knot2.pos.x);
+        let yDiff = Math.abs(knot1.pos.y - knot2.pos.y);
+
+        if( 
+            (xDiff === 0 && yDiff <=  1) || 
+            (yDiff === 0 && xDiff <=  1) || 
+            (xDiff === 1 && yDiff === 1) 
         ) {
             return true;
         } else {
@@ -120,6 +115,18 @@ class Rope {
     }
 
 }
+
+
+/**
+ * Models a Knot. 
+ * 🪢 really needed in the end...
+ */
+class Knot {
+    constructor(x, y) {
+        this.pos = { x: x, y: y };
+    }
+}
+
 
 module.exports = {
     Rope
